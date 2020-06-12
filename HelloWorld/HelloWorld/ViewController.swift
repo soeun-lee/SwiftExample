1ce46e4a25d0609242407176e7eb5c33cb127a2f
soeun-lee <lastmotion4@gmail.com>
2020-06-12 19:09:24 +0900

    Update ViewController.swift

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblHello: UILabel!
+    @IBOutlet weak var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnSend(_ sender: Any) {
+        lblHello.text = "Hello, " + txtName.text! // ! 표현식은 스위프트 문법인 옵셔널 변수. 87쪽에서 배울 예정
+    }
+    
 }
 
