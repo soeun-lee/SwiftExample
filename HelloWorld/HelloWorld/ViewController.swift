c10338575a75f0c88dde8756efb46e2192c94a47
soeun-lee <lastmotion4@gmail.com>
2020-06-13 16:27:25 +0900

    Remark ViewController.swift

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var lblHello: UILabel!
-    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var lblHello: UILabel! // 출력 레이블용 아웃렛 변수
+    @IBOutlet weak var txtName: UITextField! // 이름 입력용 아웃렛 변수
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func btnSend(_ sender: Any) {
+    @IBAction func btnSend(_ sender: UIButton) {
+        // "Hello, "라는 문자열과 txtName.text의 문자열을 결합하여 lblHello.txt에 넣음
         lblHello.text = "Hello, " + txtName.text! // ! 표현식은 스위프트 문법인 옵셔널 변수. 87쪽에서 배울 예정
     }
     
