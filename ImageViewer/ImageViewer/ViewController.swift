260ec524df911f7f5c8a6364854bc3f885fc06b3
soeun-lee <lastmotion4@gmail.com>
2020-06-14 13:45:53 +0900

    Create ImageViewer

---
+++ ImageViewer/ImageViewer/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ImageViewer
+//
+//  Created by 이소은 on 2020/06/14.
+//  Copyright © 2020 soeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    }
+
+
+}
+
